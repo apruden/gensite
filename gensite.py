ca106b409092939ee6187a3274da9a396b573961
Alex Prudencio <alex.prudencio@gmail.com>
2014-08-14 23:24:54 -0500

    refactored gensite

--- gensite.py
+++ gensite.py
@@ -1,11 +1,9 @@
-import webapp2, jinja2, os, logging, StringIO
+import webapp2, jinja2, os, logging, StringIO, zipfile
 from google.appengine.ext import ndb
 from google.appengine.api import memcache
-
+from google.appengine.api.users import get_current_user, create_login_url
 
 EDITABLE = ['.html', '.css', '.json', '.js', '.txt', '.xml', '.do']
-
-
 MIME_DICT = {
 		'.html': 'text/html',
 		'.json': 'application/json',
@@ -23,7 +21,25 @@ MIME_DICT = {
 		}
 
 
-def load_template(path):
+code_cache = {}
+
+
+class Settings(ndb.Expando):
+	pass
+
+
+settings = ndb.Key(Settings, '_settings').get()
+
+
+if not settings:
+	logging.debug('Settings not found. Init.')
+	settings = Settings(id='_settings', cache_enabled=False, editors='')
+	settings.put()
+
+logging.debug('Running with settings %s', settings)
+
+
+def load_asset_content(path):
 	asset = ndb.Key(Asset, path).get()
 	if asset:
 		return asset.content
@@ -34,17 +50,22 @@ def get_mime(path):
 
 
 JINJA_SITE_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FunctionLoader(load_template),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True,
-	cache_size=0)
+		loader=jinja2.FunctionLoader(load_asset_content),
+		extensions=['jinja2.ext.autoescape'],
+		autoescape=True,
+		cache_size=0)
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
-    extensions=['jinja2.ext.autoescape'],
-    autoescape=True,
-	cache_size=0)
+		loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
+		extensions=['jinja2.ext.autoescape'],
+		autoescape=True,
+		cache_size=0)
+
+
+class AppSetting(ndb.Model):
+	name = ndb.StringProperty()
+	value = ndb.StringProperty()
 
 
 class Asset(ndb.Model):
@@ -54,87 +75,101 @@ class Asset(ndb.Model):
 
 class AssetHandler(webapp2.RequestHandler):
 	def get(self, path):
+		path, ext = self.get_path()
+
+		if 'admin' in self.request.GET:
+			return self.edit_form(load_asset_content(path) or '', ext)
+
+		rendered_asset = memcache.get(path)
+
+		if not rendered_asset:
+			logging.debug('cache miss')
+			if ext == '.html':
+				rendered_asset = self.get_rendered_asset(path)
+			else:
+				rendered_asset = ndb.Key(Asset, path).get()
+
+				if not rendered_asset:
+					self.abort(404)
+
+			if settings.cache_enabled:
+				logging.debug('adding to cache')
+				memcache.add(path, rendered_asset, 3600)
+
+		self.response.cache_control = 'public'
+		self.response.cache_control.max_age = 48000
+
+		if ext == '.do':
+			if path not in code_cache:
+				cobj = compile(rendered_asset.content, '', 'exec')
+				code_cache[path] = cobj
+
+			exec(code_cache[path], {'response': self.response}, {})
+		else:
+			self.response.headers['Content-Type'] = str(rendered_asset.mime)
+			self.response.write(rendered_asset.content)
+
+	def post(self, path):
+		path, ext = self.get_path()
+
+		if 'delete' in self.request.POST:
+			asset = ndb.Key(Asset, path).get()
+			return self.delete_asset(asset)
+
+		logging.debug('saving content %s', path)
+		uploaded = None
+
+		if hasattr(self.request.POST['asset'], 'file'):
+			uploaded = self.request.POST['asset']
+
+		self.update_asset(path, str(self.request.POST['asset']), uploaded)
+
+	def get_rendered_asset(self, path):
+		try:
+			template = JINJA_SITE_ENVIRONMENT.get_template(path)
+		except jinja2.TemplateNotFound:
+			self.abort(404)
+
+		return Asset(content=str(template.render({})), mime='text/html')
+
+	def edit_form(self, asset_content, ext):
+		user = get_current_user()
+		if not user or user.email() not in settings.editors:
+			return self.redirect(create_login_url('/'))
+
+		self.response.write(JINJA_ENVIRONMENT.get_template('upload.html').render({
+			'content': asset_content,
+			'editable': 'upload' not in self.request.GET and ext in EDITABLE}))
+
+	def get_path(self):
 		path = self.request.path
 		ext = os.path.splitext(path)[1]
-
 		if not ext:
 			path = '/index.html' if path == '/' else '%s.html' % path
 			ext = '.html'
 
-		if 'admin' in self.request.GET:
-			if 'delete' in self.request.GET:
-				asset = ndb.Key(Asset, path).get()
-				if asset:
-					asset.key.delete()
-				self.reponse.write('OK')
-				return
-			template_content = load_template(path) or ''
-			self.response.write(JINJA_ENVIRONMENT.get_template('upload.html').render({
-				'content': template_content,
-				'editable': 'upload' not in self.request.GET and ext in EDITABLE}))
+		return path, ext
+
+	def update_asset(self, path, content, uploaded=None):
+		if uploaded.filename.endswith('zip'):
+			site_zip = zipfile.ZipFile(uploaded.file,'r')
+			self._save_zipped_files(site_zip)
 			return
 
-		asset = memcache.get(path)
-
-		if not asset:
-			logging.debug('cache miss')
-			if path.endswith('.html'):
-				t = JINJA_SITE_ENVIRONMENT.get_template(path)
-
-				if not t:
-					self.response.status = 404
-					self.response.write('Not found')
-					return
-
-				asset = Asset(content=str(t.render({})), mime='text/html')
-			else:
-				asset = ndb.Key(Asset, path).get()
-
-				if not asset:
-					self.response.status = 404
-					self.response.write('Not found')
-					return
-
-
-			if os.environ['CACHE_ENABLED'].lower() != 'false':
-				logging.debug('adding to cache')
-				memcache.add(path, asset, 3600)
-
-		self.response.cache_control = 'public'
-		self.response.cache_control.max_age = 48000
-		self.response.headers['Content-Type'] = str(asset.mime)
-
-		if ext == '.do':
-			exec(asset.content, {'response': self.response}, {})
-		else:
-			self.response.headers['Content-Type'] = str(asset.mime)
-			self.response.write(asset.content)
-
-	def post(self, path):
-		import zipfile
-		path = self.request.path
-		ext = os.path.splitext(path)[1]
-
-		if not ext:
-			path = '/index.html' if path == '/' else '%s.html' % path
-
-		if hasattr(self.request.POST['asset'], 'file'):
-			logging.debug('saving file %s', path)
-			uploaded = self.request.POST['asset']
-
-			if uploaded.filename.endswith('zip'):
-				site_zip = zipfile.ZipFile(uploaded.file,'r')
-				self._save_zip_files(site_zip)
-				return
-
+		if uploaded:
 			asset = Asset(id=path, content=uploaded.file.read(), mime=get_mime(uploaded.filename))
 		else:
-			logging.debug('saving content %s', path)
-			asset = Asset(id=path, content=str(self.request.POST['asset']), mime=get_mime(path))
+			asset = Asset(id=path, content=content, mime=get_mime(path))
 
 		asset.put()
 
-	def _save_zip_files(self, site_zip):
+	def delete_asset(self, asset):
+		if asset:
+			asset.key.delete()
+
+		return 'OK'
+
+	def _save_zipped_files(self, site_zip):
 		for p in site_zip.infolist():
 			logging.debug('saving %s', p.filename)
 			asset = Asset(id= '/%s' % p.filename, content=site_zip.read(p), mime=get_mime(p.filename))
